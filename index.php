aa9a9b03535a9707a7a6d3c06400c35af7e24fea
Justin Scothorn <scothorn.2@wright.edu>
2016-01-14 20:05:20 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>About Me</title>
+        <link rel="stylesheet" type="text/css" href="./css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="./css/myStyle.css">
+    </head>
+    <body>
+        <header>
+            <h1>Justin Scothorn</h1>
+        </header>
+        
+        <main>
+            <img src="images/justin.jpg" alt="justin">
+            <p>I am a senior at Wright State University.</p>
+            <p>I grew up in West Carrollton Ohio.</p>
+            <p>I am married and have two children ages 4 and 10 months.</p>
+            
+            <table>
+                <tr>
+                    <th colspan="3">2015-2016 Courses</th>
+                </tr>
+                <tr>
+                    <th>Course Numbers</th>
+                    <th>Course Name</th> 
+                    <th>Semester</th>
+                </tr>
+                <tr>
+                    <td>CS3800-01</td>
+                    <td>Web Development II</td>
+                    <td>Spring</td>
+                </tr>
+                <tr>
+                    <td>CEG4910-01</td>
+                    <td>Tech Based Ventures</td>
+                    <td>Spring</td>
+                </tr>
+                <tr>
+                    <td>CEG4981-01</td>
+                    <td>Team Projects II</td>
+                    <td>Spring</td>
+                </tr>
+                <tr>
+                    <td>CEG4120-01</td>
+                    <td>Managing Software Development Process</td>
+                    <td>Spring</td>
+                </tr>
+            </table>
+        </main>
+        
+        <aside>
+            <h2>Who I Cheer For!</h2>
+            <ul>
+                <li><a href="http://www.bengals.com/">Cincinnati Bengals</a></li>
+                <li>Ohio State Buckeyes</li>
+                <li>Cincinnati Reds</li>
+            </ul>
+        </aside>
+        
+        <footer>
+            <p>&copy2016</p>
+        </footer>
+        
+        
+        
+        
+       
+    </body>
+</html>
